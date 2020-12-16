0f7b84b84eb76feeb7d66eb21297985ca5abd5aa
Viktoriia Fedotkina <victoria31f@gmail.com>
2020-12-16 20:45:37 +0300

    implemented saving form data in localStorage and clearing form and localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -3,46 +3,57 @@ import './App.css';
 import { countLoanAmount, countMonthlyPayment, countRequiredIncome, countOverpayment } from "../utils/formulas";
 
 function App() {
-    const [cost, setCost] = React.useState(null);
-    const [initialCost, setInitialCost] = React.useState(null);
-    const [period, setPeriod] = React.useState(null);
-    const [interestRate, setInterestRate] = React.useState(null);
-    const costRef = React.useRef();
-    const initialCostRef = React.useRef();
-    const periodRef = React.useRef();
-    const interestRateRef = React.useRef();
+    const [cost, setCost] = React.useState(localStorage.getItem('cost') || '');
+    const [initialCost, setInitialCost] = React.useState(localStorage.getItem('initialCost') || '');
+    const [period, setPeriod] = React.useState(localStorage.getItem('period') || '');
+    const [interestRate, setInterestRate] = React.useState(localStorage.getItem('interestRate') || '');
 
     const loanAmount = countLoanAmount(cost, initialCost);
     const monthlyPayment = countMonthlyPayment(loanAmount, interestRate, period);
     const requiredIncome = countRequiredIncome(monthlyPayment);
     const overPayment = countOverpayment(monthlyPayment, period, loanAmount);
 
-    const handleCostChange = () => {
-        setCost(costRef.current.value);
+    const handleCostChange = (e) => {
+        setCost(e.target.value);
     }
 
-    const handleInitialCostChange = () => {
-        setInitialCost(initialCostRef.current.value);
+    const handleInitialCostChange = (e) => {
+        setInitialCost(e.target.value);
     }
 
-    const handlePeriodChange = () => {
-        setPeriod(periodRef.current.value);
+    const handlePeriodChange = (e) => {
+        setPeriod(e.target.value);
     }
 
-    const handleInterestRateChange = () => {
-        setInterestRate(interestRateRef.current.value);
+    const handleInterestRateChange = (e) => {
+        setInterestRate(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        localStorage.setItem('cost', cost);
+        localStorage.setItem('initialCost', initialCost);
+        localStorage.setItem('period', period);
+        localStorage.setItem('interestRate', interestRate);
+    }
+
+    const handleClear = () => {
+        localStorage.clear();
+        setCost('');
+        setInitialCost('');
+        setPeriod('');
+        setInterestRate('');
     }
 
     return (
         <div className="container">
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit} >
                 <div className="form__block">
                     <span className="form__text">Стоимость недвижимости</span>
-                    <input value={cost} ref={costRef} onChange={handleCostChange} className="form__input" type="number" placeholder="10000000"/>
+                    <input value={cost} onChange={handleCostChange} className="form__input" type="number" placeholder="10000000"/>
                 </div>
                 <div className="form__block">
                     <span className="form__text">Первоначальный взнос</span>
-                    <input value={initialCost} ref={initialCostRef} onChange={handleInitialCostChange} className="form__input" type="number" placeholder="3000000"/>
+                    <input value={initialCost} onChange={handleInitialCostChange} className="form__input" type="number" placeholder="3000000"/>
                     <div className="form__percents">
                         <button className="percent-button">10%</button>
                         <button className="percent-button">15%</button>
@@ -53,15 +64,15 @@ function App() {
                 </div>
                 <div className="form__block">
                     <span className="form__text">Срок кредита</span>
-                    <input value={period} ref={periodRef} onChange={handlePeriodChange} className="form__input" type="number" placeholder="10"/>
+                    <input value={period} onChange={handlePeriodChange} className="form__input" type="number" placeholder="10"/>
                 </div>
                 <div className="form__block">
                     <span className="form__text">Кредитная ставка</span>
-                    <input value={interestRate} ref={interestRateRef} onChange={handleInterestRateChange} className="form__input" type="number" placeholder="10"/>
+                    <input value={interestRate} onChange={handleInterestRateChange} className="form__input" type="number" placeholder="10"/>
                 </div>
                 <div className="form__buttons">
                     <button type="submit" className="form__submit button">Save</button>
-                    <button className="form__clear button">Clear</button>
+                    <button type="button" className="form__clear button" onClick={handleClear}>Clear</button>
                 </div>
             </form>
 
